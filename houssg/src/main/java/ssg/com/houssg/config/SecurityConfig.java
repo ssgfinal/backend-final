97ad18ff2e985ef4b316d56bd5eb477b77bac2ae
0-ssg <dudtjr001@gmail.com>
2023-09-13 09:45:36 +0900

    error fix

--- houssg/src/main/java/ssg/com/houssg/config/SecurityConfig.java
+++ houssg/src/main/java/ssg/com/houssg/config/SecurityConfig.java
@@ -1,34 +1,25 @@
-//package ssg.com.houssg.config;
-//
-////WebSecurityConfig.java
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import ssg.com.houssg.security.JwtTokenProvider;
-//
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig{
-//
-//	@Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeRequests(auth -> auth.anyRequest().authenticated())
-//            .csrf().disable()
-//            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        // JWT 토큰 검증 필터 추가
-//        http.addFilterBefore(new JwtAuthenticationFilter(JwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-//
-//        // CORS 설정 및 기타 보안 설정
-//
-//        return http.build();
-//    }
-//}
+package ssg.com.houssg.config;
+
+//WebSecurityConfig.java
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.AllArgsConstructor;
+
+
+@EnableWebSecurity
+@AllArgsConstructor
+@Configuration
+public class SecurityConfig {
+	@Bean
+		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.authorizeRequests()
+					.anyRequest().permitAll();
+			return http.build();
+		}
+}
