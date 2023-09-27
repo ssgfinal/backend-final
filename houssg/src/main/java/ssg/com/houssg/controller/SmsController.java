5bf4f627363e3570b989450fad84e51ffde8486d
0-ssg <dudtjr001@gmail.com>
2023-09-27 16:14:21 +0900

    접속 허용 URI 추가

--- houssg/src/main/java/ssg/com/houssg/controller/SmsController.java
+++ houssg/src/main/java/ssg/com/houssg/controller/SmsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,7 @@ import ssg.com.houssg.util.VerificationCodeValidator;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("message")
 public class SmsController {
 
 	@Autowired
