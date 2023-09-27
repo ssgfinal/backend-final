e9a6d44c5a89baf897339208e8edd32b6f64d36b
0-ssg <dudtjr001@gmail.com>
2023-09-27 16:56:27 +0900

    sms 테스트 코드 추가

--- houssg/src/main/java/ssg/com/houssg/util/VerificationCodeValidator.java
+++ houssg/src/main/java/ssg/com/houssg/util/VerificationCodeValidator.java
@@ -7,7 +7,7 @@ public class VerificationCodeValidator {
     public static boolean isValidVerificationCode(HttpSession session, String Code) {
         // 세션에서 저장된 인증번호를 가져옴.
         String storedCode = (String) session.getAttribute("verificationCode");
-        
+        System.out.println(storedCode);
         // 세션에 저장된 인증번호와 사용자가 입력한 인증번호를 비교
         if (storedCode != null && storedCode.equals(Code)) {
             // 인증번호가 일치
