c6d5f55f6fbb1604b12d7fd74bb88ca4996c721a
0-ssg <dudtjr001@gmail.com>
2023-09-28 14:09:53 +0900

    유저 컨트롤러 CrossOrigin 어노테이션 추가

--- houssg/src/main/java/ssg/com/houssg/controller/UserController.java
+++ houssg/src/main/java/ssg/com/houssg/controller/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
 	private SmsUtil smsUtil;
 
 	// 로그인
-	@CrossOrigin(origins = "*")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody UserDto user) {
 		System.out.println("UserController login(UserDto user) " + new Date());
