622c4e60ba2e4b1795466c4ddf5a261fd5d2e2fc
SanBu599 <112762794+SanBu599@users.noreply.github.com>
2023-10-17 13:33:13 +0900

    검색 수정으로 이제 page를 바꾸면 잘 결과값을 잘 바꿔줍니다

--- houssg/src/main/java/ssg/com/houssg/dto/AccommodationParam.java
+++ houssg/src/main/java/ssg/com/houssg/dto/AccommodationParam.java
@@ -8,6 +8,7 @@ public class AccommodationParam {
     private int pageSize;
     private int page;
     private int start;
+    private int end;
 	public String getSearch() {
 		return search;
 	}
@@ -44,15 +45,22 @@ public class AccommodationParam {
 	public void setStart(int start) {
 		this.start = start;
 	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
 	public AccommodationParam() {
 		super();
 	}
-	public AccommodationParam(String search, String type, String select, int pageSize, int page) {
+	public AccommodationParam(String search, String type, String select, int pageSize, int page, int start) {
 		super();
 		this.search = search;
 		this.type = type;
 		this.select = select;
 		this.pageSize = pageSize;
 		this.page = page;
+		this.start = start;
 	}
 }
